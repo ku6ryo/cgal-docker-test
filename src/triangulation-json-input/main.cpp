d98581567d5c03c59ac12427246f114f77fe75b3
Ryo Kuroyanagi <im.ryokuro@gmail.com>
2023-07-23 01:12:06 +0900

    added indices

--- src/triangulation-json-input/main.cpp
+++ src/triangulation-json-input/main.cpp
@@ -7,9 +7,10 @@
 #include <CGAL/Triangulation_vertex_base_with_info_2.h>
 #include <CGAL/Constrained_Delaunay_triangulation_2.h>
 #include <CGAL/Polygon_2.h>
+#include <map>
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-typedef CGAL::Triangulation_vertex_base_with_info_2<int, K> Vb;
+typedef CGAL::Triangulation_vertex_base_with_info_2<unsigned, K> Vb;
 typedef CGAL::Constrained_triangulation_face_base_2<K> Fb;
 typedef CGAL::Triangulation_data_structure_2<Vb, Fb> TDS;
 typedef CGAL::Exact_predicates_tag Itag;
@@ -18,55 +19,80 @@ typedef CGAL::Polygon_2<K> Polygon_2;
 
 using json = nlohmann::json;
 
-int main() {
+int main(int argc, char **argv)
+{
 
-    json j;
-    std::ifstream inFile("../json/contour.json");
+    // get the first argument as the input file name
+    if (argc < 2)
+    {
+        std::cout << "Usage: " << argv[0] << " <input file>" << std::endl;
+        return 1;
+    }
+    std::string inputFileName = argv[1];
 
-    if (!inFile) {
+    json j_input;
+    std::ifstream inFile(inputFileName);
+
+    if (!inFile)
+    {
         std::cout << "Error reading file" << std::endl;
         return 1;
     }
-    inFile >> j;
+    inFile >> j_input;
     inFile.close();
 
     Polygon_2 polygon;
-    for (const auto& element : j["points"]) {
+    std::map<K::Point_2, unsigned> point_indices;
+    for (const auto &element : j_input["points"])
+    {
         float x = element[0].get<float>();
         float y = element[1].get<float>();
-        std::cout << "X: " << x << ", Y: " << y << std::endl;
-        polygon.push_back(K::Point_2(x, y));
+        K::Point_2 p = K::Point_2(x, y);
+        polygon.push_back(p);
+        point_indices[p] = polygon.size() - 1;
     }
 
     CDT cdt;
     cdt.insert_constraint(polygon.vertices_begin(), polygon.vertices_end(), true);
 
-    json output;
-    json triangles = json::array();
-    for (CDT::Finite_faces_iterator fit=cdt.finite_faces_begin(); fit!=cdt.finite_faces_end();++fit) {
+    json j_output;
+    json j_points = json::array();
+    json j_indices = json::array();
+    for (CDT::Finite_faces_iterator fit = cdt.finite_faces_begin(); fit != cdt.finite_faces_end(); ++fit)
+    {
         CDT::Triangle t = cdt.triangle(fit);
 
         float cx = (t.vertex(0).x() + t.vertex(1).x() + t.vertex(2).x()) / 3;
         float cy = (t.vertex(0).y() + t.vertex(1).y() + t.vertex(2).y()) / 3;
 
-        if (polygon.bounded_side(K::Point_2(cx, cy)) == CGAL::ON_BOUNDED_SIDE) {
-            json triangle = {
+        if (polygon.bounded_side(K::Point_2(cx, cy)) == CGAL::ON_BOUNDED_SIDE)
+        {
+            unsigned i0 = point_indices[fit->vertex(0)->point()];
+            unsigned i1 = point_indices[fit->vertex(1)->point()];
+            unsigned i2 = point_indices[fit->vertex(2)->point()];
+
+            json indices = {i0, i1, i2};
+            j_indices.push_back(indices);
+            json points = {
                 {t.vertex(0).x(), t.vertex(0).y()},
                 {t.vertex(1).x(), t.vertex(1).y()},
-                {t.vertex(2).x(), t.vertex(2).y()}
-            };
-            triangles.push_back(triangle);
+                {t.vertex(2).x(), t.vertex(2).y()}};
+            j_points.push_back(points);
         }
     }
-    output["triangles"] = triangles;
-    std::string outputJsonStr = output.dump();
+    j_output["points"] = j_points;
+    j_output["indices"] = j_indices;
+    std::string outputJsonStr = j_output.dump();
     std::cout << outputJsonStr << std::endl;
 
     std::fstream outFile("triangles.json", std::ios::out | std::ios::trunc);
-    if (outFile.is_open()) {
+    if (outFile.is_open())
+    {
         outFile << outputJsonStr;
         outFile.close();
-    } else {
+    }
+    else
+    {
         std::cerr << "Unable to open file";
     }
 }
